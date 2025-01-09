aed44a44ca8cb4bcb35f22783768b1ef6cd14350
Maris Shardyko <marrttaoburberry@gmail.com>
2025-01-09 19:26:47 +0200

    completed task 2

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 using namespace std;
 
-struct name {
-private:
+struct point {
+	int x;
+	int y;
 
-public:
+	void distance(point p) {
+		cout << "Distance between points: " << sqrt(pow(p.x - x, 2) + pow(p.y - y, 2)) << endl;
+	}
 };
 
 int main() {
-
+	point p1, p2;
+	cout << "Enter x and y coordinates of point 1: ";
+	cin >> p1.x >> p1.y;
+	cout << "Enter x and y coordinates of point 2: ";
+	cin >> p2.x >> p2.y;
+	p1.distance(p2);
+	return 0;
 }
